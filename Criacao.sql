d62d040cc285bc4f95368aa13b8c825d21eaeb1a
Giordano <giordano@suffert.com>
2019-11-14 23:38:30 -0300

    Seleção de Engine para FK

--- Criacao.sql
+++ Criacao.sql
@@ -19,29 +19,29 @@ CREATE TABLE if not exists `personagem` (
   `fk_usr_nick` varchar(255) NOT NULL,
   PRIMARY KEY (`nome`),
   FOREIGN KEY (fk_usr_nick) REFERENCES usuario(email)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE if not exists `vestimenta` (
   `id_vest` int(11) NOT NULL auto_increment,
   `cor_prim_vest` int(11) NOT NULL,
   `cor_sec_vest` int(11) NOT NULL,
   PRIMARY KEY (`id_vest`)
-);
+)ENGINE=InnoDB;
 
-alter table vestimenta add column disney integer not null default 66 after id_vest;
-alter table vestimenta drop column disney;
+--alter table vestimenta add column disney integer not null default 66 after id_vest;
+--alter table vestimenta drop column disney;
 
-alter table vestimenta modify column cor_prim_vest int(16);
+--alter table vestimenta modify column cor_prim_vest int(16);
 
-alter table vestimenta change column cor_prim cor_prim_vest int(32) not null default 0;
+--alter table vestimenta change column cor_prim cor_prim_vest int(32) not null default 0;
 
-drop table usuario;
+--drop table usuario;
 
-insert into personagem values ('lordjordan2', 'M', 5, 5, 5, 5, 'giordano@orion.br');
+--insert into personagem values ('lordjordan2', 'M', 5, 5, 5, 5, 'giordano@orion.br');
 
-insert into usuario values ('GiordanoSM', 'giordano@orion.br', 'senha');
+--insert into usuario values ('GiordanoSM', 'giordano@orion.br', 'senha');
 
-describe vestimenta;
+--describe vestimenta;
 
-select * from personagem;
+--select * from personagem;
 
